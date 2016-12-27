39ceea2522d2256a77f3b63fe5a0717aa8e6bcc6
Pranav Revankar <pranavrevankar007@gmail.com>
2016-12-27 05:08:40 +0000

    create article and the form

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
   #get 'pages/home', to:'pages#home'
-  get 'pages/about', to: 'pages#about'
+  #get 'pages/about', to: 'pages#about'
   get 'info', to: 'info'
+  
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
